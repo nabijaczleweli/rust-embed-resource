7a5f6c0c0c2bca268e44af3883ec7aa79bf8d384
nabijaczleweli <nabijaczleweli@gmail.com>
2017-06-10 07:42:51 +0200

    Also scan Win10-specific places, maybe
    Ref: #5

--- src/windows_msvc.rs
+++ src/windows_msvc.rs
@@ -1,8 +1,18 @@
 use std::path::{PathBuf, Path};
 use std::process::Command;
 use winreg::enums::*;
-use std::env;
+use std::{env, fs};
 use winreg;
+
+macro_rules! try_opt {
+    ($opt:expr) => {
+        if let Some(o) = $opt {
+            o
+        } else {
+            return None;
+        }
+    }
+}
 
 pub const SUPPORTED: bool = true;
 
@@ -31,6 +41,7 @@ fn find_windows_sdk_rc_exe() -> Option<PathBuf> {
         .or_else(|| find_windows_kits_rc_exe("KitsRoot81", arch))
         .or_else(|| find_windows_kits_rc_exe("KitsRoot", arch))
         .or_else(|| find_latest_windows_sdk_rc_exe(arch))
+        .or_else(|| find_windows_10_kits_rc_exe("KitsRoot10", arch))
 }
 
 // Windows 8 - 10
@@ -51,6 +62,29 @@ fn find_latest_windows_sdk_rc_exe(arch: Arch) -> Option<PathBuf> {
         .ok()
         .and_then(|root_dir| try_bin_dir(root_dir, "Bin", "Bin/x64", arch))
         .and_then(try_rc_exe)
+}
+
+// Windows 10 with subdir support
+fn find_windows_10_kits_rc_exe(key: &str, arch: Arch) -> Option<PathBuf> {
+    let root_dir = try_opt!(winreg::RegKey::predef(HKEY_LOCAL_MACHINE)
+        .open_subkey_with_flags(r"SOFTWARE\Microsoft\Windows Kits\Installed Roots", KEY_QUERY_VALUE)
+        .and_then(|reg_key| reg_key.get_value::<String, _>(key))
+        .ok()) + "/bin";
+
+    for entry in try_opt!(fs::read_dir(&root_dir).ok()).filter(|d| d.is_ok()).map(Result::unwrap) {
+        let fname = entry.file_name().into_string();
+        let ftype = entry.file_type();
+        if fname.is_err() || ftype.is_err() || ftype.unwrap().is_file() {
+            continue;
+        }
+
+        let fname = entry.file_name().into_string().unwrap();
+        if let Some(rc) = try_bin_dir(root_dir.clone(), &format!("{}/x86", fname), &format!("{}/x64", fname), arch).and_then(try_rc_exe) {
+            return Some(rc);
+        }
+    }
+
+    None
 }
 
 fn try_bin_dir(root_dir: String, x86_bin: &str, x64_bin: &str, arch: Arch) -> Option<PathBuf> {
