384a4df1177503ce5f7ff0ea2eeab623552489cf
nabijaczleweli <nabijaczleweli@gmail.com>
2017-08-13 09:35:20 +0200

    Panic if windres fails, analogously to RC.EXE

--- src/windows_not_msvc.rs
+++ src/windows_not_msvc.rs
@@ -3,9 +3,12 @@ use std::process::Command;
 pub const SUPPORTED: bool = true;
 
 pub fn compile_resource(out_dir: &str, prefix: &str, resource: &str) {
-    Command::new("windres")
+    if !Command::new("windres")
         .args(&["--input", resource, "--output-format=coff", "--output"])
         .arg(&format!("{}/lib{}.a", out_dir, prefix))
         .status()
-        .expect("Failed to execute windres");
+        .expect("Failed to execute windres")
+        .success() {
+        panic!("windres failed to compile specified resource file");
+    }
 }
