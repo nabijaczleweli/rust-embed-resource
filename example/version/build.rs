149274b5a44ed3210d826635aefa9e3e9e06025e
Michael Farrell <micolous+git@gmail.com>
2023-06-25 18:54:47 +0930

    add example with version as more realistic test

---
+++ example/version/build.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+    if !cfg!(disable_windows_manifest) && target_os == "windows" {
+        embed_resource::compile("version.rc", &["VERSION=\"0.5.0\""]);
+    }
+}
