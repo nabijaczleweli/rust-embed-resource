e83a9bf7f257623a057e2f8bcf59be77abf93cff
Michael Farrell <micolous+git@gmail.com>
2023-06-25 17:51:33 +0930

    Add real embedding test. Validate it on MSVC/{aarch64,amd64,i686} on GHA
    Closes: #60

---
+++ .github/workflows/msvc/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pefile, sys
+
+pe = pefile.PE(sys.argv[1], fast_load=True)
+pe.parse_data_directories(directories=[pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_RESOURCE"],])
+fi_strings = pe.FileInfo[0][0].StringTable[0].entries
+
+# must match version.rc
+print(f"version strings: {fi_strings}")
+assert fi_strings == {b"CompanyName": b"nabijaczleweli", b"ProductName": b"rust-embed-resource/example/version", b"ProductVersion": b"2.1.1"}
