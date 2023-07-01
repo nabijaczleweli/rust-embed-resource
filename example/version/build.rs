0e506b09f4182e422d0296423ec989880e595000
наб <nabijaczleweli@nabijaczleweli.xyz>
2023-07-01 16:06:49 +0200

    msvc

--- example/version/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-
-    if !cfg!(disable_windows_resources) && target_os == "windows" {
-        embed_resource::compile("version.rc", embed_resource::NONE);
-    }
-}
