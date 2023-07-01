80ebf1cbb6af30705aa72e2bad8ba007045d2f28
наб <nabijaczleweli@nabijaczleweli.xyz>
2023-07-01 15:38:05 +0200

    q

--- example/version/test.py
+++ example/version/test.py
@@ -2,11 +2,9 @@
 import pefile, sys
 
 pe = pefile.PE(sys.argv[1], fast_load=True)
-pe.parse_data_directories(directories=[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_RESOURCE'],])
+pe.parse_data_directories(directories=[pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_RESOURCE"],])
 fi_strings = pe.FileInfo[0][0].StringTable[0].entries
 
 # must match version.rc
 print(f"version strings: {fi_strings}")
-assert fi_strings[b'CompanyName'] == b'nabijaczleweli'
-assert fi_strings[b'ProductName'] == b'rust-embed-resource/example/version'
-assert fi_strings[b'ProductVersion'] == b'2.1.1'
+assert fi_strings == {b"CompanyName": b"nabijaczleweli", b"ProductName": b"rust-embed-resource/example/version", b"ProductVersion": b"2.1.1"}
