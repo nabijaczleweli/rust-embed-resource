62da115ce6a6b4b4e47c65a698b533785e945b8c
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-09-19 17:39:08 +0200

    usage: rust-embed-resource resource [include-dir]
    Ref: #81

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,12 @@ fn main() {
 
     let mut args = env::args_os();
     let argv0 = args.next().map(Cow::from).unwrap_or(Cow::from(OsStr::new("rust-embed-resource")));
-    let resource = args.next().unwrap_or_else(|| panic!("usage: {} resource", Path::new(&*argv0).display()));
-    embed_resource::compile(&resource, &["VERSION=\"0.5.0\""]).manifest_required().unwrap();
-    embed_resource::compile_for(&resource, &["embed_resource", "embed_resource-installer"], embed_resource::NONE).manifest_required().unwrap();
+    let resource = args.next().unwrap_or_else(|| panic!("usage: {} resource [include-dir]", Path::new(&*argv0).display()));
+    let include_dir = args.next();
+    embed_resource::compile(&resource, embed_resource::ParamsMacrosAndIncludeDirs(["VERSION=\"0.5.0\""], include_dir.as_ref())).manifest_required().unwrap();
+    embed_resource::compile_for(&resource,
+                                ["embed_resource", "embed_resource-installer"],
+                                embed_resource::ParamsIncludeDirs(include_dir))
+        .manifest_required()
+        .unwrap();
 }
