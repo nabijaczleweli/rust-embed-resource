nabijaczleweli/rust-embed-resource

commit d72b4496810f5c2adea9a2647ec1dd9ffa047f7c

Build with llvm-rc on non-MSVC Win32 with target aarch64.*gnullvm
Supersedes #88

src/windows_not_msvc.rs
@@ -1,6 +1,6 @@
-use self::super::{ParameterBundle, apply_parameters};
+use self::super::ParameterBundle;
+use self::super::windres::*;
 use std::path::{PathBuf, MAIN_SEPARATOR};
-use std::process::Command;
 use std::borrow::Cow;
 use std::ffi::OsStr;
 use std::env;
@@ -24,8 +24,6 @@ impl ResourceCompiler {
     pub fn compile_resource<Ms: AsRef<OsStr>, Mi: IntoIterator<Item = Ms>, Is: AsRef<OsStr>, Ii: IntoIterator<Item = Is>>(
         &self, out_dir: &str, prefix: &str, resource: &str, parameters: ParameterBundle<Ms, Mi, Is, Ii>)
         -> Result<String, Cow<'static, str>> {
-        let out_file = format!("{}{}lib{}.a", out_dir, MAIN_SEPARATOR, prefix);
-
         // Under some msys2 environments, $MINGW_CHOST has the correct target for
         // GNU windres or llvm-windres (clang32, clang64, or clangarm64)
         let target = env::var_os("MINGW_CHOST").map(Cow::Owned).unwrap_or_else(|| {
@@ -39,22 +37,38 @@ impl ResourceCompiler {
                 .into()
         });
 
-        match apply_parameters(Command::new("windres")
-                                   .args(&["--input", resource, "--output-format=coff", "--target"])
-                                   .arg(target)
-                                   .args(&["-c", "65001"]) // UTF-8, cf. https://github.com/nabijaczleweli/rust-embed-resource/pull/73
-                                   .args(&["--output", &out_file, "--include-dir", out_dir]),
-                               "-D",
-                               "-I",
-                               parameters)
-            .status() {
-            Ok(stat) if stat.success() => Ok(out_file),
-            Ok(stat) => Err(format!("windres failed to compile \"{}\" into \"{}\" with {}", resource, out_file, stat).into()),
-            Err(e) => Err(format!("Couldn't to execute windres to compile \"{}\" into \"{}\": {}", resource, out_file, e).into()),
-        }
+        Compiler::choose(&target).compile(out_dir,
+                                          prefix,
+                                          format!("{}{}lib{}.a", out_dir, MAIN_SEPARATOR, prefix),
+                                          resource,
+                                          parameters,
+                                          "-fo",
+                                          "-C",
+                                          "-no-preprocess",
+                                          |c| {
+                                              c.arg("--target")
+                                                  .arg(target)
+                                                  .args(&["-c", "65001"]) // UTF-8, cf. https://github.com/nabijaczleweli/rust-embed-resource/pull/73
+                                          })
     }
 }
 
+impl Compiler {
+    pub fn choose(target: &OsStr) -> Compiler {
+        match target.as_encoded_bytes() {
+            // "aarch64".."gnullvm"
+            // https://github.com/llvm/llvm-project/issues/125371
+            [b'a', b'a', b'r', b'c', b'h', b'6', b'4', .., b'g', b'n', b'u', b'l', b'l', b'v', b'm'] => Compiler::llvm_rc(OsStr::new("llvm-rc").into()),
+
+            _ => {
+                Compiler {
+                    tp: CompilerType::WindRes,
+                    executable: OsStr::new("windres").into(),
+                }
+            }
+        }
+    }
+}
 
 pub fn find_windows_sdk_tool_impl(_: &str) -> Option<PathBuf> {
     None

Cargo.toml
@@ -48,13 +48,15 @@ exclude = ["*.enc"]
 [dependencies]
 rustc_version = "0.4"
 toml = "1"
+cc = "1.2"
 
 [target.'cfg(not(target_os = "windows"))'.dependencies]
 memchr = "2.7"
-cc = "1.2"
+
+[target.'cfg(all(target_os = "windows", not(target_env = "msvc")))'.dependencies]
+memchr = "2.7"
 
 [target.'cfg(all(target_os = "windows", target_env = "msvc"))'.dependencies]
-cc = "1.2"
 vswhom = "0.1"
 
 [target.'cfg(all(target_os = "windows", target_env = "msvc"))'.dependencies.winreg]

src/lib.rs
@@ -133,9 +133,8 @@
 #![allow(private_bounds)]
 
 
-#[cfg(any(not(target_os = "windows"), all(target_os = "windows", target_env = "msvc")))]
 extern crate cc;
-#[cfg(not(target_os = "windows"))]
+#[cfg(any(not(target_os = "windows"), all(target_os = "windows", not(target_env = "msvc"))))]
 extern crate memchr;
 #[cfg(all(target_os = "windows", target_env = "msvc"))]
 extern crate vswhom;
@@ -627,4 +626,139 @@ fn env_target_and_rc() -> Result<(String, Option<OsString>), Cow<'static, str>> 
     let target = env::var("TARGET").map_err(|_| Cow::from("no $TARGET"))?;
     let rc = env::var_os(&format!("RC_{}", target)).or_else(|| env::var_os(&format!("RC_{}", target.replace('-', "_")))).or_else(|| env::var_os("RC"));
     Ok((target, rc))
+}
+
+
+// #[allow(unused)]
+mod windres {
+    use self::super::{ParameterBundle, apply_parameters};
+    use std::process::{Command, Stdio};
+    use std::borrow::Cow;
+    use std::path::Path;
+    use std::ffi::OsStr;
+    use memchr::memmem;
+    use std::fs;
+
+    #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub enum CompilerType {
+        /// LLVM-RC
+        ///
+        /// Requires a separate C preprocessor step on the source RC file
+        LlvmRc { has_no_preprocess: bool, },
+        /// MinGW windres
+        WindRes,
+    }
+
+    #[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct Compiler {
+        pub tp: CompilerType,
+        pub executable: Cow<'static, OsStr>,
+    }
+
+    impl Compiler {
+        pub fn llvm_rc(executable: Cow<'static, OsStr>) -> Self {
+            Compiler {
+                tp: CompilerType::LlvmRc {
+                    has_no_preprocess: Command::new(&executable)
+                        .arg("/?")
+                        .output()
+                        .ok()
+                        .map(|out| memmem::find(&out.stdout, b"no-preprocess").is_some())
+                        .unwrap_or(false),
+                },
+                executable: executable,
+            }
+        }
+
+        pub fn compile<Ms: AsRef<OsStr>, Mi: IntoIterator<Item = Ms>, Is: AsRef<OsStr>, Ii: IntoIterator<Item = Is>, Wp: FnOnce(&mut Command) -> &mut Command>(
+            &self, out_dir: &str, prefix: &str, out_file: String, resource: &str, parameters: ParameterBundle<Ms, Mi, Is, Ii>, fo: &str, c: &str,
+            no_preprocess: &str, windres_params: Wp)
+            -> Result<String, Cow<'static, str>> {
+            match self.tp {
+                CompilerType::LlvmRc { has_no_preprocess } => {
+                    let preprocessed_path = format!("{}/{}-preprocessed.rc", out_dir, prefix);
+                    fs::write(&preprocessed_path,
+                              cc_xc(apply_parameters_cc(cc::Build::new().define("RC_INVOKED", None), parameters))
+                                  .file(resource)
+                                  .cargo_metadata(false)
+                                  .include(out_dir)
+                                  .expand()).map_err(|e| e.to_string())?;
+
+                    try_command(Command::new(&self.executable)
+                                .args(&[fo, &out_file])
+                                .args(&[c, "65001"]) // UTF-8, cf. https://github.com/nabijaczleweli/rust-embed-resource/pull/73
+                                .args(if has_no_preprocess {
+                                    // We already preprocessed using CC. llvm-rc preprocessing
+                                    // requires having clang in PATH, which more exotic toolchains
+                                    // may not necessarily have.
+                                    Some(no_preprocess)
+                                } else {
+                                    None
+                                })
+                                .args(&["--", &preprocessed_path])
+                                .stdin(Stdio::piped())
+                                .current_dir(or_curdir(Path::new(resource).parent().expect("Resource parent nonexistent?"))),
+                                Path::new(&self.executable),
+                                "compile",
+                                &preprocessed_path,
+                                &out_file)?;
+                }
+                CompilerType::WindRes => {
+                    try_command(apply_parameters(windres_params(Command::new(&self.executable)
+                                                     .args(&["--input", resource, "--output", &out_file, "--include-dir", out_dir, "--output-format=coff"])),
+                                                 "-D",
+                                                 "-I",
+                                                 parameters),
+                                Path::new(&self.executable),
+                                "compile",
+                                resource,
+                                &out_file)?;
+                }
+            }
+            Ok(out_file)
+        }
+    }
+
+
+    fn apply_parameters_cc<'t, Ms: AsRef<OsStr>, Mi: IntoIterator<Item = Ms>, Is: AsRef<OsStr>, Ii: IntoIterator<Item = Is>>(to: &'t mut cc::Build,
+                                                                                                                             parameters: ParameterBundle<Ms,
+                                                                                                                                                         Mi,
+                                                                                                                                                         Is,
+                                                                                                                                                         Ii>)
+                                                                                                                             -> &'t mut cc::Build {
+        for m in parameters.macros {
+            let mut m = m.as_ref().to_str().expect("macros must be UTF-8 in this configuration").splitn(2, '=');
+            to.define(m.next().unwrap(), m.next());
+        }
+        for id in parameters.include_dirs {
+            to.include(id.as_ref());
+        }
+        to
+    }
+
+    fn cc_xc(to: &mut cc::Build) -> &mut cc::Build {
+        if to.get_compiler().is_like_msvc() {
+            // clang-cl
+            to.flag("-Xclang");
+        }
+        to.flag("-xc");
+        to
+    }
+
+    fn try_command(cmd: &mut Command, exec: &Path, action: &str, whom: &str, whre: &str) -> Result<(), Cow<'static, str>> {
+        match cmd.status() {
+            Ok(stat) if stat.success() => Ok(()),
+            Ok(stat) => Err(format!("{} failed to {} \"{}\" into \"{}\" with {}", exec.display(), action, whom, whre, stat).into()),
+            Err(e) => Err(format!("Couldn't execute {} to {} \"{}\" into \"{}\": {}", exec.display(), action, whom, whre, e).into()),
+        }
+    }
+
+    fn or_curdir(directory: &Path) -> &Path {
+        if directory == Path::new("") {
+            Path::new(".")
+        } else {
+            directory
+        }
+    }
+
 }

src/non_windows.rs
@@ -1,10 +1,9 @@
-use self::super::{ParameterBundle, env_target_and_rc, apply_parameters};
-use std::process::{Command, Stdio};
+use self::super::{ParameterBundle, env_target_and_rc};
+use self::super::windres::*;
 use std::ffi::{OsString, OsStr};
 use std::path::{PathBuf, Path};
 use std::borrow::Cow;
-use std::{fs, mem};
-use memchr::memmem;
+use std::mem;
 
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
@@ -31,26 +30,18 @@ impl ResourceCompiler {
     pub fn compile_resource<Ms: AsRef<OsStr>, Mi: IntoIterator<Item = Ms>, Is: AsRef<OsStr>, Ii: IntoIterator<Item = Is>>(
         &self, out_dir: &str, prefix: &str, resource: &str, parameters: ParameterBundle<Ms, Mi, Is, Ii>)
         -> Result<String, Cow<'static, str>> {
-        self.compiler.as_ref().expect("Not supported but we got to compile_resource()?").compile(out_dir, prefix, resource, parameters)
+        self.compiler.as_ref().expect("Not supported but we got to compile_resource()?").compile(out_dir,
+                                                                                                 prefix,
+                                                                                                 format!("{}/{}.lib", out_dir, prefix),
+                                                                                                 resource,
+                                                                                                 parameters,
+                                                                                                 "/fo",
+                                                                                                 "/C",
+                                                                                                 "/no-preprocess",
+                                                                                                 |c| c)
     }
 }
 
-
-#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
-enum CompilerType {
-    /// LLVM-RC
-    ///
-    /// Requires a separate C preprocessor step on the source RC file
-    LlvmRc { has_no_preprocess: bool, },
-    /// MinGW windres
-    WindRes,
-}
-
-#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
-struct Compiler {
-    tp: CompilerType,
-    executable: Cow<'static, OsStr>,
-}
 
 impl Compiler {
     pub fn probe() -> Result<Compiler, Cow<'static, str>> {
@@ -62,122 +53,22 @@ impl Compiler {
         if target.ends_with("-windows-gnu") || target.ends_with("-windows-gnullvm") {
             let executable = format!("{}-w64-mingw32-windres", &target[0..target.find('-').unwrap_or_default()]);
             if is_runnable(&executable) {
-                return Ok(Compiler {
+                Ok(Compiler {
                     tp: CompilerType::WindRes,
                     executable: OsString::from(executable).into(),
-                });
+                })
             } else {
-                return Err(executable.into());
+                Err(executable.into())
             }
         } else if target.ends_with("-windows-msvc") {
             if is_runnable("llvm-rc") {
-                return Ok(Compiler {
-                    tp: CompilerType::LlvmRc {
-                        has_no_preprocess: Command::new("llvm-rc")
-                            .arg("/?")
-                            .output()
-                            .ok()
-                            .map(|out| memmem::find(&out.stdout, b"no-preprocess").is_some())
-                            .unwrap_or(false),
-                    },
-                    executable: OsStr::new("llvm-rc").into(),
-                });
+                Ok(Compiler::llvm_rc(OsStr::new("llvm-rc").into()))
             } else {
-                return Err("llvm-rc".into());
+                Err("llvm-rc".into())
             }
+        } else {
+            Err("".into())
         }
-
-        Err("".into())
-    }
-
-    pub fn compile<Ms: AsRef<OsStr>, Mi: IntoIterator<Item = Ms>, Is: AsRef<OsStr>, Ii: IntoIterator<Item = Is>>(&self, out_dir: &str, prefix: &str,
-                                                                                                                 resource: &str,
-                                                                                                                 parameters: ParameterBundle<Ms, Mi, Is, Ii>)
-                                                                                                                 -> Result<String, Cow<'static, str>> {
-        let out_file = format!("{}/{}.lib", out_dir, prefix);
-        match self.tp {
-            CompilerType::LlvmRc { has_no_preprocess } => {
-                let preprocessed_path = format!("{}/{}-preprocessed.rc", out_dir, prefix);
-                fs::write(&preprocessed_path,
-                          cc_xc(apply_parameters_cc(cc::Build::new().define("RC_INVOKED", None), parameters))
-                              .file(resource)
-                              .cargo_metadata(false)
-                              .include(out_dir)
-                              .expand()).map_err(|e| e.to_string())?;
-
-                try_command(Command::new(&self.executable)
-                                .args(&["/fo", &out_file])
-                                .args(&["/C", "65001"]) // UTF-8, cf. https://github.com/nabijaczleweli/rust-embed-resource/pull/73
-                                .args(if has_no_preprocess {
-                                    // We already preprocessed using CC. llvm-rc preprocessing
-                                    // requires having clang in PATH, which more exotic toolchains
-                                    // may not necessarily have.
-                                    &["/no-preprocess"][..]
-                                } else {
-                                    &[][..]
-                                })
-                                .args(&["--", &preprocessed_path])
-                                .stdin(Stdio::piped())
-                                .current_dir(or_curdir(Path::new(resource).parent().expect("Resource parent nonexistent?"))),
-                            Path::new(&self.executable),
-                            "compile",
-                            &preprocessed_path,
-                            &out_file)?;
-            }
-            CompilerType::WindRes => {
-                try_command(apply_parameters(Command::new(&self.executable)
-                                                 .args(&["--input", resource, "--output-format=coff", "--output", &out_file, "--include-dir", out_dir]),
-                                             "-D",
-                                             "--include-dir",
-                                             parameters),
-                            Path::new(&self.executable),
-                            "compile",
-                            resource,
-                            &out_file)?;
-            }
-        }
-        Ok(out_file)
-    }
-}
-
-fn apply_parameters_cc<'t, Ms: AsRef<OsStr>, Mi: IntoIterator<Item = Ms>, Is: AsRef<OsStr>, Ii: IntoIterator<Item = Is>>(to: &'t mut cc::Build,
-                                                                                                                         parameters: ParameterBundle<Ms,
-                                                                                                                                                     Mi,
-                                                                                                                                                     Is,
-                                                                                                                                                     Ii>)
-                                                                                                                         -> &'t mut cc::Build {
-    for m in parameters.macros {
-        let mut m = m.as_ref().to_str().expect("macros must be UTF-8 in this configuration").splitn(2, '=');
-        to.define(m.next().unwrap(), m.next());
-    }
-    for id in parameters.include_dirs {
-        to.include(id.as_ref());
-    }
-    to
-}
-
-fn cc_xc(to: &mut cc::Build) -> &mut cc::Build {
-    if to.get_compiler().is_like_msvc() {
-        // clang-cl
-        to.flag("-Xclang");
-    }
-    to.flag("-xc");
-    to
-}
-
-fn try_command(cmd: &mut Command, exec: &Path, action: &str, whom: &str, whre: &str) -> Result<(), Cow<'static, str>> {
-    match cmd.status() {
-        Ok(stat) if stat.success() => Ok(()),
-        Ok(stat) => Err(format!("{} failed to {} \"{}\" into \"{}\" with {}", exec.display(), action, whom, whre, stat).into()),
-        Err(e) => Err(format!("Couldn't execute {} to {} \"{}\" into \"{}\": {}", exec.display(), action, whom, whre, e).into()),
-    }
-}
-
-fn or_curdir(directory: &Path) -> &Path {
-    if directory == Path::new("") {
-        Path::new(".")
-    } else {
-        directory
     }
 }
 

src/windows_msvc.rs
@@ -57,8 +57,8 @@ enum Arch {
 
 pub fn find_windows_sdk_tool_impl(tool: &str) -> Option<PathBuf> {
     let arch = match env::var_os("HOST").expect("No HOST env var").as_encoded_bytes() {
-        [b'x', b'8', b'6', b'_', b'6', b'4', ..] => Arch::X64, // "x86_64"
-        [b'a', b'a', b'r', b'c', b'h', b'6', b'4', ..] => Arch::AArch64, // "aarch64"
+        [b'x', b'8', b'6', b'_', b'6', b'4', _..] => Arch::X64, // "x86_64"
+        [b'a', b'a', b'r', b'c', b'h', b'6', b'4', _..] => Arch::AArch64, // "aarch64"
         _ => Arch::X86,
     };
 

commit 5819ec1331a9188570d1532f2b5aa2e2e247d9a1

Only try llvm-rc if windres is llvm-windres on non-windows when building for arm64
Fixes: d72b4496810f5c2adea9a2647ec1dd9ffa047f7c
Fixes: https://github.com/nabijaczleweli/rust-embed-resource/commit/d72b4496810f5c2adea9a2647ec1dd9ffa047f7c#commitcomment-194257398

src/windows_not_msvc.rs
@@ -1,6 +1,7 @@
 use self::super::ParameterBundle;
 use self::super::windres::*;
 use std::path::{PathBuf, MAIN_SEPARATOR};
+use std::process::Command;
 use std::borrow::Cow;
 use std::ffi::OsStr;
 use std::{env, mem};
@@ -70,10 +71,19 @@ impl Compiler {
         match target.as_encoded_bytes() {
             // "aarch64".."gnullvm"
             // https://github.com/llvm/llvm-project/issues/125371
-            [b'a', b'a', b'r', b'c', b'h', b'6', b'4', .., b'g', b'n', b'u', b'l', b'l', b'v', b'm'] => Compiler::llvm_rc("llvm-rc".into()),
+            [b'a', b'a', b'r', b'c', b'h', b'6', b'4', .., b'g', b'n', b'u', b'l', b'l', b'v', b'm'] if !windres_is_gnu_windres() => {
+                Compiler::llvm_rc("llvm-rc".into())
+            }
 
             _ => Compiler::windres("windres".into()),
         }
+    }
+}
+
+fn windres_is_gnu_windres() -> bool {
+    match Command::new("windres").args(&["-V", "/?"]).output() {
+        Ok(out) => !out.stdout.starts_with(b"llvm-windres"),
+        Err(_) => false,
     }
 }
 